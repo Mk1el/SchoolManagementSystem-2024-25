c2916db6491e85ec29500426d086c39fc1ccd574
Mk1el <moseskilonzo2001@gmail.com>
2025-06-25 10:04:06 +0300

    Added functionality to the sidebar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,18 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+  CommonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatButtonModule
+],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  todayDate: string = '';
+  user: any = {
+    username: '',
+    role: ''
+  };
+
   constructor(private router: Router) {}
 
-  logout() {
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
+  ngOnInit(): void {
+    // Format date: e.g. Monday, 24 June 2025
+    const today = new Date();
+    this.todayDate = today.toLocaleDateString('en-US', {
+      weekday: 'long',
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    });
+
+    const userData = localStorage.getItem('auth_user');
+    if (userData) {
+      const parsed = JSON.parse(userData);
+      this.user.username = parsed.username;
+      this.user.role = parsed.role;
+    }
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/']);
   }
 }
-
